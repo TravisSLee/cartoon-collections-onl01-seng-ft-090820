2373a978d09d640d7665b0d247babfad2cca28f2
Travis Lee <travisslee223@gmail.com>
2020-09-11 22:31:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,7 +28,7 @@ def find_the_cheese(foods) # code an argument here
   i = 0
   cheese_types = ["cheddar", "gouda", "camembert"]
   while i < cheese_types.length
-    return cheese_types[i] if array.include?(cheese_types[i])
+    return cheese_types[i] if foods.include?(cheese_types[i])
     i += 1
   end
 end
